3fb4bfb9b400c0edaf61c7e17680118ff64e7738
CamiloFuentesR <lokomilo1@gmail.com>
2021-04-15 21:37:43 -0400

    f

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom'
-import { CalendarApp } from '../CalendarApp'
 import { LoginScreen } from '../components/auth/LoginScreen'
 import { CalendarScreen } from '../components/calendar/CalendarScreen'
 
